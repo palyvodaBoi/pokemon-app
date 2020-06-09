ab4333b6594ce00c8da274f520ad1fa0df7051d7
palyvodaBoi <xpalyvodax@gmail.com>
2020-06-09 18:33:27 +0300

    Updates

---
+++ precache-manifest.f81dbf99757ce4dc8cafc3c92bd22dd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b177517d3bcf4bf6aa6b615cad2fa8d",
+    "url": "/palyvodaBoi/pokemon-app.git/index.html"
+  },
+  {
+    "revision": "eb29b2757b26174c6411",
+    "url": "/palyvodaBoi/pokemon-app.git/static/css/main.e1b739a1.chunk.css"
+  },
+  {
+    "revision": "a4f1a350d4dd8c25eefd",
+    "url": "/palyvodaBoi/pokemon-app.git/static/js/2.cbb70fe7.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/palyvodaBoi/pokemon-app.git/static/js/2.cbb70fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb29b2757b26174c6411",
+    "url": "/palyvodaBoi/pokemon-app.git/static/js/main.20a35754.chunk.js"
+  },
+  {
+    "revision": "f91840361769630fff97",
+    "url": "/palyvodaBoi/pokemon-app.git/static/js/runtime-main.f0bddc1e.js"
+  }
+]);
