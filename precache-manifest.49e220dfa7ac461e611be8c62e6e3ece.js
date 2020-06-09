50799ebbcab78de29ad210e8c0687b019694b632
palyvodaBoi <xpalyvodax@gmail.com>
2020-06-10 01:07:24 +0300

    Updates

---
+++ precache-manifest.49e220dfa7ac461e611be8c62e6e3ece.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b760964c5c2ae72948e7fbc501d84a",
+    "url": "/pokemon-app/index.html"
+  },
+  {
+    "revision": "ecd6c213493ca3b49341",
+    "url": "/pokemon-app/static/css/main.e1b739a1.chunk.css"
+  },
+  {
+    "revision": "a4f1a350d4dd8c25eefd",
+    "url": "/pokemon-app/static/js/2.cbb70fe7.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/pokemon-app/static/js/2.cbb70fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecd6c213493ca3b49341",
+    "url": "/pokemon-app/static/js/main.f11166e9.chunk.js"
+  },
+  {
+    "revision": "f1ede30f39791275cc78",
+    "url": "/pokemon-app/static/js/runtime-main.1e65d33b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/palyvodaBoi/pokemon-app.git/precache-manifest.f81dbf99757ce4dc8cafc3c92bd22dd5.js"
+  "/pokemon-app/precache-manifest.49e220dfa7ac461e611be8c62e6e3ece.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/palyvodaBoi/pokemon-app.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokemon-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
